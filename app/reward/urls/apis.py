36152bb5434822e6e8f4c1e0cfd39f2f42694e98
sungmin414 <sungmin3231@gmail.com>
2018-08-17 18:15:40 +0900

    수정

--- app/reward/urls/apis.py
+++ app/reward/urls/apis.py
@@ -6,8 +6,18 @@ urlpatterns = [
     path('', apis.ProductList.as_view()),
     path('search/', apis.ProductCategoryList.as_view()),
     path('<int:pk>/', apis.ProductDetail.as_view()),
+<<<<<<< HEAD
     path('<int:pk>/funding/', apis.ProductFundingList.as_view()),
     # path('item/', apis.RewardList.as_view()),
     # path('search/', apis.ProductFilterList.as_view()),
     # path('search/product/', apis.ProductOrderingList.as_view()),
+=======
+
+    path('item/', apis.RewardList.as_view()),
+    path('search/', apis.ProductFilterList.as_view()),
+
+
+
+
+>>>>>>> filter 수정
 ]
