a6fc6401e197d4655e14de8b74d440d7c63c930c
dohwan <dosio0102@gmail.com>
2018-08-17 17:38:26 +0900

    email 인증

--- app/members/models.py
+++ app/members/models.py
@@ -1,6 +1,9 @@
+import os
+
 from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
 from django.db import models
 # Create your models here.
+from config.settings.base import STATIC_DIR
 
 
 class UserManager(DjangoUserManager):
@@ -8,7 +11,12 @@ class UserManager(DjangoUserManager):
 
 
 class User(AbstractUser):
-    img_profile = models.ImageField(upload_to='user', blank=True)
+    blank_image_path = os.path.join(STATIC_DIR, 'static')
+    print(blank_image_path)
+    img_profile = models.ImageField(
+        upload_to='user',
+        default=blank_image_path
+    )
 
     nickname = models.CharField(max_length=16)
 
